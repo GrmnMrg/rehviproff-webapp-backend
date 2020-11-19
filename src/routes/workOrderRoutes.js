d6eb89a5920577cc2dacff73abacf5e10d450b8f
German Mumma <german.mumma@ttu.ee>
2020-11-19 13:47:25 +0200

    Add workorder API operations

--- src/routes/workOrderRoutes.js
+++ src/routes/workOrderRoutes.js
@@ -4,18 +4,15 @@ const WorkOrder = require('../models/WorkOrder');
 const User = require('../models/User');
 const logger = require('../Logger');
 
-router.get('/', verify, async (req, res) => {
+router.get('/', async (req, res) => {
 
 	let workOrders = [];
+	logger.info(`Getting all work orders`);
 
-	if (req.body.order_id) {
-		workOrders[0] = await WorkOrder.findById(req.body.order_id);
-	} else {
-		workOrders = await WorkOrder.find();
-	}
 
 	// Try and get and send back all work orders
 	try {
+		workOrders = await WorkOrder.find();
 		logger.info('WorkOrder getting successful');
 		res.send(workOrders);
 	} catch(err) {
@@ -25,29 +22,56 @@ router.get('/', verify, async (req, res) => {
 
 });
 
-router.post('/save', verify, async (req, res) => {
+router.get('/:order_id', async (req, res) => {
+
+	let workOrders = [];
+	const orderId = req.params.order_id;
+
+		logger.info(`Getting work order with id ${orderId}`);
+		try{
+			workOrders[0] = await WorkOrder.findById(orderId);
+			res.status(200).send(workOrders);
+		}catch(err){
+			logger.error('WorkOrder getting failed: ' + err);
+			res.status(400).send({
+				"message": `Error getting workorder with id ${orderId}`,
+				"error": err.message
+			});
+		}
+});
+
+router.post('/save', async (req, res) => {
 
 	const user = await User.findOne({ _id: req.user });
 
 	// Create a new work order
 	const workOrder = new WorkOrder({
-		creator_user_id: user._id,
-		creator_user_username: user.username,
-		title: req.body.title || 'missing'
+		date: req.body.date,
+		client_name_and_address: req.body.client_name_and_address,
+		client_phone: req.body.client_phone,
+		car_mark_and_model: req.body.car_mark_and_model,
+		car_reg_nr: req.body.car_reg_nr,
+		car_vin_code: req.body.car_vin_code,
+		car_mileage: req.body.car_mileage,
+		list_of_works: req.body.list_of_works,
+		replaceable_parts: req.body.replaceable_parts,
+		faults_and_remarks: req.body.faults_and_remarks,
+		required_parties_client_representative: req.body.required_parties_client_representative,
+		required_parties_work_receiver: req.body.required_parties_work_receiver,
+		required_parties_work_performer: req.body.required_parties_work_performer
 	});
 
 	// Try and save the new work order
 	try {
 		const savedWorkOrder = await workOrder.save();
 		logger.info('Work order save successful');
-		res.send({ 
-			workOrder_id: workOrder._id,
-			creator_user_id: workOrder.creator_user_id,
-			creator_user_username: workOrder.creator_user_username,
-			title: workOrder.title
+
+		res.send({
+			"message": "Workorder saved successfully",
+			"data": savedWorkOrder
 		});
 	} catch(err) {
-		logger.error('Workorder save failed' + err);
+		logger.error('Workorder save failed: ' + err);
 		res.status(400).send(err);
 	}
 
