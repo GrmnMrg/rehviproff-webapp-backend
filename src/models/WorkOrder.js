f1530ff77e018382658e70b79cb3c9776d3c10bd
German Mumma <german.mumma@ttu.ee>
2020-11-19 13:46:54 +0200

    Add workorder model properties

--- src/models/WorkOrder.js
+++ src/models/WorkOrder.js
@@ -1,25 +1,56 @@
 const mongoose = require('mongoose');
 
 const workOrderSchema = new mongoose.Schema({
-	creator_user_id: {
-		type: String,
-		requiered: true,
-		min: 3,
-		max: 254 
-	},
-	creator_user_username: {
-		type: String,
-		requiered: true,
-		min: 3,
-		max: 254
-	},
-	title: {
-		type: String,
-		max: 254 
-	},
 	date: {
 		type: Date,
 		default: Date.now
+	},
+	client_name_and_address: {
+		type: String,
+		max: 512
+	},
+	client_phone: {
+		type: String,
+		max: 254
+	},
+	car_mark_and_model: {
+		type: String,
+		max: 254
+	},
+	car_reg_nr: {
+		type: String,
+		max: 254
+	},
+	car_vin_code: {
+		type: String,
+		max: 254
+	},
+	car_mileage: {
+		type: Number
+	},
+	list_of_works: {
+		type: String,
+		max: 2048
+	},
+	replaceable_parts: {
+		type: String,
+		max: 2048
+	},
+	faults_and_remarks: {
+		type: String,
+		max: 2048
+	},
+	required_parties_client_representative: {
+		type: String,
+		max: 512
+	},
+	required_parties_work_receiver: {
+		type: String,
+		max: 512
+	},
+	required_parties_work_performer: {
+		type: String,
+		max: 512
 	}
 });
 
